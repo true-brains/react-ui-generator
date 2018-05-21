cd790d315fc81526f33775e4959629e0d2a19876
Alexei Zaviruha <a.zaviruha@gmail.com>
2018-05-21 15:28:33 +0300

    Add  HOF to exported functions

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,6 +1,7 @@
 export { GeneratedForm } from './components/GeneratedForm';
 export { Field } from './components/Field';
 export { Fields } from './components/Fields';
+export { withFields } from './components/Layout';
 export { withDefaults, getMetaById, extractFieldActions } from './utils';
 export { Validator, buildValidator } from './validator';
 export { serializeToObject, buildJSONSerializer } from './serializer';
