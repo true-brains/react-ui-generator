e29f10913eee37b6096e0ba1fd1e7ed806e276dc
Alexei Zaviruha <a.zaviruha@gmail.com>
2019-07-20 16:00:15 +0300

    chore: add webpack build for Metaphor package

---
+++ packages/metaphor/config/webpack.config.js
@@ -0,0 +1,50 @@
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+const resolve = dir => path.join(__dirname, '..', dir);
+
+module.exports = {
+  entry: resolve('src/index.ts'),
+  output: {
+    filename: 'metaphor.js',
+    path: resolve('out'),
+    libraryTarget: 'umd'
+  },
+
+  devtool: 'source-map',
+  mode: 'production',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    modules: [resolve('src'), 'node_modules'],
+    plugins: [new TsConfigPathsPlugin({})]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+          'awesome-typescript-loader'
+        ],
+        exclude: /node_modules/
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+
+  plugins: [new CleanWebpackPlugin()]
+};
